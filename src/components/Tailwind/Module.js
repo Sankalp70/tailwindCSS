28eb9e8d5ed9aae2b4e88b7d166c737a435615a4
Sankalp Ramteke <sankalprt658@gmail.com>
2024-04-24 12:55:49 +0530

    icons adjusted

--- src/components/Tailwind/Module.js
+++ src/components/Tailwind/Module.js
@@ -4,6 +4,9 @@ import { FaRegUserCircle } from "react-icons/fa";
 import { RiSearchLine } from "react-icons/ri";
 import { MdOutlineKeyboardArrowDown } from "react-icons/md";
 import bgimg from "../../assets/bgimg.jpg";
+import { BsCalculatorFill } from "react-icons/bs";
+import { RiGiftFill } from "react-icons/ri";
+import { FaLocationDot } from "react-icons/fa6";
 
 const Module = () => {
   return (
@@ -54,19 +57,19 @@ const Module = () => {
         <header className="relative z-10 p-4 md:p-8 text-white text-center">
           {" "}
           {/* Content over the image */}
-          <h1 className="text-2xl md:text-5xl text-black mt-16">
+          <h1 className="text-2xl md:text-6xl text-black mt-16">
             Ship, Manage, Track, Deliver
           </h1>{" "}
           {/* Responsive text size */}
-          <div className="flex justify-center items-center h-screen ">
-            <div className="flex  w-40 h-40 text-black text-3xl text-justify bg-white items-center justify-center">
-              RATE&SHIP{" "}
+          <div className="flex justify-center items-center h-screen relative bottom-60 text-xl font-bold">
+            <div className="flex  w-40 h-40 text-black text-justify bg-white items-center justify-center">
+            <BsCalculatorFill /> RATE&SHIP{" "} 
             </div>
-            <div className="flex  w-52 h-52 text-3xl text-justify bg-violet-700 text-white items-center justify-center">
-              TRACK
+            <div className="flex  w-52 h-52 text-justify bg-violet-700 text-white items-center justify-center">
+            <RiGiftFill />   TRACK 
             </div>
-            <div className="flex  w-40 h-40 text-black text-3xl text-justify bg-white items-center justify-center">
-              LOCATIONS
+            <div className="flex  w-40 h-40 text-black  text-justify bg-white items-center justify-center">
+            <FaLocationDot />    LOCATIONS
             </div>
           </div>
         </header>
